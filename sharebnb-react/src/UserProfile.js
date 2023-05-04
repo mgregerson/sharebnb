18232f18ea5804f3889e9930972735a056944740
Brian Joo <brianhjoo@gmail.com>
2023-05-04 12:33:44 -0700

    made changes to UserProfile comp.

--- sharebnb-react/src/UserProfile.js
+++ sharebnb-react/src/UserProfile.js
@@ -4,13 +4,14 @@ import { useNavigate } from "react-router-dom";
  *
  */
 function UserProfile({ user }) {
-  const navigate = useNavigate()
 
   return (
     <div className='UserProfile'>
       <h1>Welcome John Doe </h1>
-      <ul>{}</ul>
-      <button onClick={() => navigate(`/rentals/${user.username}/add`)} >Add a rental</button>
+      <ul>{user.rentals}</ul>
+      <button onClick={useNavigate(`/rentals/${user.username}/add`)} >
+        Add a rental
+      </button>
     </div>
   )
 }
