efa4d6a2ff2963cccf1b6e0f414769d72fa8d02f
mgregerson <mgregerson4321@gmail.com>
2023-06-20 18:05:16 -0400

    Refactor ShareBNB API Class from JS to Typescript

--- sharebnb-react/src/api.ts
+++ sharebnb-react/src/api.ts
@@ -1,38 +1,42 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import jwt_decode from "jwt-decode";
+import { JwtPayload } from "jwt-decode";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5001"; // ;
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5001";
 
-class shareBnbApi {
+class ShareBnbApi {
   static token = "";
+
   // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" +
   // ".eyJ1c2VybmFtZSI6IkpvaG4gU21pdGgifQ" +
   // ".t_lzxBsYc6BNfwldAFC3LWEsmqd2TI-s6dqtbJ7VNZk";
   // static token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVC
   // J9.eyJ1c2VybmFtZSI6ImpvaG5fZG9lIn0.EnOOQMZOPEZ2tm0Z-lhS38IqsOK-Wt_bDbHeQuQ2N7A`
 
-  static async request(endpoint, data = {}, method = "get") {
-    // console.debug("API Call:", endpoint, data, method, this.token);
-
+  static async request(
+    endpoint: string,
+    data: any = {},
+    method: string = "get"
+  ) {
     const url = `${BASE_URL}/${endpoint}`;
-    const headers = { Authorization: `Bearer ${shareBnbApi.token}` };
+    const headers = { Authorization: `Bearer ${ShareBnbApi.token}` };
     const params = method === "get" ? data : {};
 
     try {
-      return (await axios({ url, method, data, params, headers })).data;
-    } catch (err) {
+      const config: AxiosRequestConfig = { url, method, data, params, headers };
+      const response = await axios(config);
+      return response.data;
+    } catch (err: any) {
       console.log("err:", err);
       console.error("API Error:", err.response);
-      let message = err.response.data.error.message;
-      throw Array.isArray(message) ? message : [message];
+      let message = err.response?.data?.error?.message;
+      throw Array.isArray(message)
+        ? message
+        : [message || "Unknown error occurred."];
     }
   }
 
-  // Auth
-
-  /** POST Register new user function */
-
-  static async registerUser(inputData) {
+  static async registerUser(inputData: any) {
     const { username, password, email, image_url, bio, location } = inputData;
     let res = await this.request(
       "signup",
@@ -44,59 +48,46 @@ class shareBnbApi {
     return this.token;
   }
 
-  /** POST Login user */
-
-  static async loginUser(inputData) {
+  static async loginUser(inputData: any) {
     const { username, password } = inputData;
     let res = await this.request(`login`, { username, password }, "post");
     this.token = res.token;
     return this.token;
   }
 
-  /** GET Gets user data */
-
   static async getAllRentals() {
     let res = await this.request(`rentals`);
     return res.rentals;
   }
 
-  /** GET Gets user data */
-
-  static async getUser(username) {
+  static async getUser(username: string) {
     let res = await this.request(`users/${username}`);
     return res.user;
   }
 
-  /** GET All user rentals */
-
-  static async getRentals(inputData) {
-    const { username } = jwt_decode(this.token);
-
+  static async getRentals(inputData: any) {
+    const decodedToken = jwt_decode<JwtPayload>(this.token);
+    const { username } = decodedToken as { username: string };
     let res = await this.request(`rentals/${username}`);
     return res.rentals;
   }
 
-  /** Get a rental by ID */
-
-  static async getRental(id) {
+  static async getRental(id: number) {
     let res = await this.request(`/rentals/${id}`);
     return res.rental;
   }
 
-  /** Get All rentals of another user. */
-
-  static async getRentalsForUser(username) {
+  static async getRentalsForUser(username: string) {
     let res = await this.request(`rentals/${username}`);
     return res.rentals;
   }
-  /** POST Add new rental */
 
-  static async addRental(inputData) {
-    const { username } = jwt_decode(this.token);
-
+  static async addRental(inputData: any) {
+    const decodedToken = jwt_decode<JwtPayload>(this.token);
+    const { username } = decodedToken as { username: string };
     let res = await this.request(`rentals/${username}/add`, inputData, "post");
     return res.rental;
   }
 }
 
-export default shareBnbApi;
+export default ShareBnbApi;
